6d155371eb8c1d4c47b206ee2740075bd2cca066
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-14 23:01:01 +0300

    .

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,49 @@
-console.log('04 Store API')
+require('dotenv').config()
+
+
+// async errors
+
+
+
+
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+
+// middleware
+
+app.use(express.json());
+
+
+
+//rootes
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products"></a>');
+});
+
+
+// products route
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
+const port = process.env.PORT || 3000;
+
+const start = async() => {
+    try {
+        app.listen(port, console.log(`server is listening on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+start()
