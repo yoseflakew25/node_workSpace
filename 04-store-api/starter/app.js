30b99d15af8babd0a7f1f03b64efca87d56f7d50
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-15 00:04:04 +0300

    .

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -45,6 +45,7 @@ const port = process.env.PORT || 3000;
 
 const start = async() => {
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`server is listening on port ${port}`))
     } catch (error) {
         console.log(error)
