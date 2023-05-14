eb70bad26540044f33914afae047d958b5e2db30
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-14 15:50:45 +0300

    .

--- TASK-MANAGER/starter/models/Task.js
+++ TASK-MANAGER/starter/models/Task.js
@@ -6,7 +6,9 @@ const mongoose = require('mongoose')
 const taskSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'must provide name'],
+        trim: true,
+        maxlength: [20, 'name can not be more than 20 characters']
     },
     completed: {
         type: Boolean,
