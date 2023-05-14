de057f45b7d48c552ebf730541c20b2473450075
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-15 00:13:08 +0300

    .

--- 04-store-api/starter/routes/products.js
+++ 04-store-api/starter/routes/products.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
+
+
+const { getAllProductsStatic, getAllProductsStatic, getAllProducts } = require('../controllers/products');
+
+
+
+router.route('/').get(getAllProducts)
+router.route('/static').get(getAllProductsStatic)
