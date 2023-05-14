f21c223fb7fbaa65385358ebb05b9968310523c3
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-14 12:55:06 +0300

    .

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect')
+require('dotenv').config()
 
 //middleware
 
@@ -28,7 +29,7 @@ const port = 3000
 
 const start = async() => {
     try {
-        await connectDB()
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, () => {
             console.log(`server listening on port ${port}...`)
         })
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 
-const connectionString = 'mongodb+srv://jolarohe:1qaz2wsx@nodeexpressprojects.iu4wngv.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority'
 
 const connectDB = (url) => {
-    return mongoose.connect(connectionString, {
+    return mongoose.connect(url, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
