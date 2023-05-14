e756760d3c3362d84f9e7b8b03c98a0644df60b7
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-14 14:12:18 +0300

    .

--- 03-task-manager/final/app.js
+++ 03-task-manager/final/app.js
@@ -19,15 +19,15 @@ app.use(notFound);
 app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
 
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
