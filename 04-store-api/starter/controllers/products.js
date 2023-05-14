9ccac58359d4bfbcac3cc40860d76ac6fd0e60bc
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-15 00:10:58 +0300

    .

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -3,6 +3,13 @@ const getAllProductsStatic = async(req, res) => {
 }
 
 
-const getAllProducts = async(req, res) => {
+const getAllProducts = async (req, res) => {
     res.status(200).json({ msg: 'products route' })
+}
+
+
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic
 }
