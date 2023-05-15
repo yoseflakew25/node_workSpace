beaac0fd1b6de8c9c9d25bf265f0135ab2975377
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-15 09:46:04 +0300

    .

---
+++ 04-store-api/starter/models/product.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+
+
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'product name must be provided'],
+    },
+    price: {
+        type: Number,
+        required: [true, 'product price must be provided'],
+    },
+    description: {
+        type: String,
+        required: [true, 'product description must be provided'],
+    },
+    image: {
+        type: String,
+        required: [true, 'product image must be provided'],
+    },
+    category: {
+        type: String,
+        required: [true, 'product category must be provided'],
+    },
+    quantity: {
+        type: Number,
+        required: [true, 'product quantity must be provided'],
+    },
+})
