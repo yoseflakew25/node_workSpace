72a2a686269a963f38cd55361266595133f6b826
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-15 13:40:48 +0300

    .

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -14,6 +14,16 @@ const getAllProductsStatic = async(req, res) => {
 
 
 const getAllProducts = async (req, res) => {
+    const {featured} = req.query
+    const queryObject={}
+
+
+
+        if (featured){
+            queryObject.featured=featured==='true'?true:false
+        }
+    
+    console.log(queryObject);
     const products=await product.find(req.query)
     res.status(200).json({products, nbHits:products.length})
 
