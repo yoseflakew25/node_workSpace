0365312d5426d84819a825b9df55f6695ca54cd5
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-15 10:30:41 +0300

    .

--- 04-store-api/starter/populate.js
+++ 04-store-api/starter/populate.js
@@ -1,22 +1,22 @@
 require('dotenv').config();
 
-
 const connectDB = require('./db/connect');
-const product = require('./models/product');
-
-
-
+const Product = require('./models/product');
 const jsonProducts = require('./products.json');
 
-
-
-
-const start = async() => {
+const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
         // await product.create(jsonProducts);
-        console.log('Database connected');
+       
+        await Product.deleteMany();
+        await Product.create(jsonProducts);
+        console.log('Success');
     } catch (error) {
         console.log(error);
     }
 }
+
+
+
+start()
